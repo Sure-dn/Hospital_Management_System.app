9896da4deafc93f8bd27d46adf76446358aafcc8
sure <sure@123gmail.com>
2026-04-15 15:38:18 +0530

    changes made

--- src/main/java/com/sprint/project/RepositoryClasses/BlockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.project.RepositoryClasses;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sprint.project.EntityClasses.Block;
-
-public interface BlockRepository extends JpaRepository<Block,Integer> {
-
-}
