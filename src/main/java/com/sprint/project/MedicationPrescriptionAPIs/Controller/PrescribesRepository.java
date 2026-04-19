446274491e0e8db39c38c9fe34aafdecd02191a6
Chrisjohn <Christopher007john@gmail.com>
2026-04-19 19:58:57 +0530

    all files added

---
+++ src/main/java/com/sprint/project/MedicationPrescriptionAPIs/Controller/PrescribesRepository.java
@@ -0,0 +1,27 @@
+package com.sprint.project.MedicationPrescriptionAPIs.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import com.sprint.project.MedicationPrescriptionAPIs.Entity.PrescribesEntity;
+import com.sprint.project.MedicationPrescriptionAPIs.Entity.PrescribesId;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PrescribesRepository extends JpaRepository<PrescribesEntity, PrescribesId> {
+
+    // Custom Query 1: Prescriptions by Patient SSN
+    @Query("SELECT p FROM PrescribesEntity p WHERE p.patient.ssn = :ssn")
+    List<PrescribesEntity> findByPatientSsn(@Param("ssn") Integer ssn);
+
+    // Custom Query 2: Prescriptions by Physician EmployeeID
+    @Query("SELECT p FROM PrescribesEntity p WHERE p.physician.employeeId = :employeeId")
+    List<PrescribesEntity> findByPhysicianEmployeeId(@Param("employeeId") Integer employeeId);
+
+    // Custom Query 3: Prescriptions by Medication Code
+    @Query("SELECT p FROM PrescribesEntity p WHERE p.medication.code = :code")
+    List<PrescribesEntity> findByMedicationCode(@Param("code") Integer code);
+
+	List<PrescribesEntity> findByDateBetween(LocalDateTime from, LocalDateTime to);
+}
