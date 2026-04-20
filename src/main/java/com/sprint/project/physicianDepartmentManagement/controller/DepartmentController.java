b42d171783cd6e6ddc245fb6c0ca43c9687c42de
Sure-dn <sspremalatha3@gmail.com>
2026-04-20 13:19:56 +0530

    Update DepartmentController.java

--- src/main/java/com/sprint/project/physicianDepartmentManagement/controller/DepartmentController.java
+++ src/main/java/com/sprint/project/physicianDepartmentManagement/controller/DepartmentController.java
@@ -1,4 +1,4 @@
-package com.sprint.project.physicianDepartmentManagement.controller;
+package com.sprint.project.physicianDepartmentManagement.Controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
