2894a6d1f1e5590d538598686f8b92f3feb43b56
sure-dn <sspremalatha3@gmail.com>
2026-04-25 00:21:41 +0530

    changes made in frontend

--- src/main/java/com/sprint/project/ProjectApplication.java
+++ src/main/java/com/sprint/project/ProjectApplication.java
@@ -1,32 +1,14 @@
 package com.sprint.project;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import com.sprint.project.authorization.security.JwtFilter;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.sprint.project")
 public class ProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
-
-    // ✅ REGISTER JWT FILTER
-    @Bean
-    public JwtFilter jwtFilter() {
-        return new JwtFilter();
-    }
-
-    @Bean
-    public FilterRegistrationBean<JwtFilter> filterRegistrationBean() {
-        FilterRegistrationBean<JwtFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(jwtFilter());
-        registration.addUrlPatterns("/api/*");
-        return registration;
-    }
 }
+
